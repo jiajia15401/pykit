ea074231fdf6fecb43972ac169b7373e1ac4c233
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-09 10:50:27 -0500

    Create load.py

---
+++ pykit/load.py
@@ -0,0 +1,19 @@
+from pykit.decode import decode
+class load():
+    def __init__(self,path = None,class_def = None):
+        self.path = path
+        self.use = class_def
+    def str(self,name):
+        n = name + '.date'
+        if not self.path is None:
+            n = self.path + n
+        s = open(n,'r')
+        if not self.use() is None:
+            self.use(s)
+        else:
+            n = decode()._2_to_str(s.readline())
+            if n == decode()._16_to_str(s.readline()):
+                if hash(n) == s.readline():
+                    return n
+            else:
+                return 'can find %s' % name
