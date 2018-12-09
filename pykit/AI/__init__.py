935157c0893a4fb7ec2738f13d73e0bc6f3d6697
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-09 10:52:42 -0500

    Create __init__.py

---
+++ pykit/AI/__init__.py
@@ -0,0 +1,35 @@
+import importlib
+__all__ = ['ai']
+v = '1.0.0'
+class ai():
+    def make(self,path):
+        n = path + '.py'
+        self.name = n
+        s = open(n ,'w')
+        run = '''class ai():
+    name = "%s"
+    def __init__(self):
+        self.list = []
+        self.add = []
+    def why(self):
+        pass
+    def think(self):
+        self.why()
+    def use_work(self):
+        now = []
+        while True:
+            for i in self.add
+                i()
+                now.append('%s 1' % i)
+                for o in self.goal:
+                    if not o():
+                        list_of_goal("%s %s0" % now o)
+            list_of_goal("%s 1" % now)
+            self.think()
+    def add(self,add):
+        self.add.append(add)
+    def list_of_goal(self,goal):
+        self.list.append(goal)
+'''% name
+        s.writelines (run)
+        return ai()
