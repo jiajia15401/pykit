6b8f611702dca33a72684cb0e62a8299916f9e2a
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-24 08:48:43 -0500

    Update cmd_make.py

--- pywidget/cmd_make.py
+++ pywidget/cmd_make.py
@@ -1,8 +1,17 @@
 from os import system,popen
-import time
 import threading
 __all__ = ['cmd_run', 'cmd']
-def cmd_run(code,how_print = print()):
+global myline
+class myline(threading.Thread):   
+    def __init__(self, n):
+        threading.Thread.__init__(self)
+        self.threadID = 11011
+        self.name = 'cmd'
+        self.counter = 123321
+        self.n = n
+    def run(self):               
+        self.cmd(self.n[0], self.n[1], self.n[2], '(self.if_stop == 1)or(%s)'% self.n[3])
+def cmd_run(code,how_print = print):
     try:
         f = os.popen(code)
         how_print(f.read())
@@ -12,24 +21,27 @@ def run_cmd(self,input_thing,how_print,how_input,way_stop):
     self.if_stop = 0
     while True:
         a = how_input(input_thing)
-        if (way_stop)or(self.if_stop == 1):
+        if (way_stop):
             break
         cmd_run(a, how_print)
+myline.cmd = run_cmd
 class cmd():
-    def __init__(self,input_thing,how_print = print,how_input = input,way_stop = None):
-        class myline(threading.Thread):   
-            def __init__(self, anser):
-                threading.Thread.__init__(self)
-                self.threadID = 11011
-                self.name = 'cmd'
-                self.counter = 123321
-                self.anser = anser
-            def run(self):               
-                n = self.anser
-                run_cmd(n[0],n[1] ,n[2],n[3]) 
-        cmd = myline([input_thing,how_print,how_input,way_stop])
-        cmd.start()
+    class cmd(myline):
+        pass
+    def start(self,input_thing = '>>',how_print = print,how_input = input,way_stop = None):
+        n = [input_thing, how_print, how_input, way_stop]
+        myline(n)
+        myline().start()
+        return myline()
     def stop(self):
         self.if_stop = 1
-        cmd.join()
+        self.cmd().close()
+        self.cmd().join()
+if __name__ == '__main__':
+    cmd().start()
+    __import__('time')
+    print('it will stop in 100s')
+    time.sleep(100)
+    cmd().stop()
+    
     
