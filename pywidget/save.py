025e9eae2b317dd5b4ea35825ce27a24423c1a81
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-24 08:55:08 -0500

    Update save.py

--- pywidget/save.py
+++ pywidget/save.py
@@ -9,18 +9,43 @@ class use_to_decode():
     def _2_to_str(self,s):
         return ''.join([chr(i) for i in [int(b, 2) for b in s.split(' ')]])
 class save():
-    def __init__(self,path = None,class_def = None):
+    
+    def __init__(self,path = None,class_ = None,last_name = '.data'):
         self.path = path
-        self.use = class_def
-    def str(self,name,value):
-        n = name + '.date'
-        if not self.path is None:
-            n = self.path + n
-        s = open(n,'w')
-        v = value
-        if not self.use is None:
-            s.writelines(self.use(v))
+        self.use = class_
+        self.name = last_name
+    def str(self,name,value,last_name = None):
+        def str_(self,name,value,last_name = self.name):
+            n = name + last_name
+            if not self.path is None:
+                n = self.path + n
+            s = open(n,'w')
+            v = value
+            if not self.use.str is None:
+                s.writelines(self.use().str(v))
+            else:
+                s.writelines(use_to_decode.str_to_2(use_to_decode,v))
+                s.writelines(use_to_decode.str_to_16(use_to_decode,v))
+                s.writelines(hash(v))
+        if last_name == None:
+            str_(name,value)
         else:
-            s.writelines(use_to_decode.str_to_2(use_to_decode,v))
-            s.writelines(use_to_decode.str_to_16(use_to_decode,v))
-            s.writelines(hash(v))
+            str_(name,value,last_name)
+    def list(self,name,value,last_name = None):
+        def list_(self,name,value,last_name = '.list' + self.name):
+            n = name + last_name
+            if not self.path is None:
+                n = self.path + n
+            s = open(n,'w')
+            v = value
+            if not self.use.list is None:
+                s.writelines(self.use().list(v))
+            else:
+                for i in v:
+                    s.writelines(use_to_decode.str_to_2(use_to_decode,i))
+                    s.writelines(use_to_decode.str_to_16(use_to_decode,i))
+                    s.writelines(hash(i))
+        if last_name == None:
+            self.make(name,value)
+        else:
+            self.make(name,value,last_name)
