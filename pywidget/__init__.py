ed4296aab0ce1a5ec01ef1dedaf3a1cec53ad0d3
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-25 16:59:35 -0500

    Update __init__.py

--- pywidget/__init__.py
+++ pywidget/__init__.py
@@ -3,6 +3,7 @@ from pywidget import save
 from pywidget import load
 from pywidget import error
 from pywidget import AI
-__all__ = ['error', 'save', 'load', 'decode','line']
+__all__ = ['error','cmd_make', 'save', 'load', 'decode','line','sencise']
 __author__ = 'john wan <jwan@ps163m.org>[www.jiajia15401.ml]'
 version = '1.4.6'
+name = 'pywidget©️'
