afdf24f390ff8ffed1a2c32bf12569c60679e8e3
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-25 16:55:29 -0500

    Update cmd_make.py

--- pywidget/cmd_make.py
+++ pywidget/cmd_make.py
@@ -1,16 +1,6 @@
 from os import system,popen
 import threading
 __all__ = ['cmd_run', 'cmd']
-global myline
-class myline(threading.Thread):   
-    def __init__(self, n):
-        threading.Thread.__init__(self)
-        self.threadID = 11011
-        self.name = 'cmd'
-        self.counter = 123321
-        self.n = n
-    def run(self):               
-        self.cmd(self.n[0], self.n[1], self.n[2], '(self.if_stop == 1)or(%s)'% self.n[3])
 def cmd_run(code,how_print = print):
     try:
         f = os.popen(code)
@@ -24,24 +14,23 @@ def run_cmd(self,input_thing,how_print,how_input,way_stop):
         if (way_stop):
             break
         cmd_run(a, how_print)
-myline.cmd = run_cmd
-class cmd():
-    class cmd(myline):
-        pass
-    def start(self,input_thing = '>>',how_print = print,how_input = input,way_stop = None):
-        n = [input_thing, how_print, how_input, way_stop]
-        myline(n)
-        myline().start()
-        return myline()
+class cmd(threading.Thread):
+    def __init__(self,input_thing = '>>',how_print = print,how_input = input,way_stop = None):
+        self.n = [input_thing,how_print,how_input,way_stop]
+        threading.Thread.__init__(self)
+        self.threadID = 11011
+        self.name = 'cmd'
+        self.counter = 123321
+    def run(self):
+        run_cmd(self.n[0],self.n[1],self.n[2],self.n[3])
     def stop(self):
         self.if_stop = 1
         self.cmd().close()
         self.cmd().join()
 if __name__ == '__main__':
     cmd().start()
-    __import__('time')
+    import time
     print('it will stop in 100s')
     time.sleep(100)
     cmd().stop()
-    
     
