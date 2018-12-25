c5fd0f634ccefc08218f206e5b13d2dda12dd647
pykit <45291841+jiajia15401@users.noreply.github.com>
2018-12-24 19:37:43 -0500

    Update sencise.py

--- pywidget/sencise.py
+++ pywidget/sencise.py
@@ -1,4 +1,3 @@
-from numpy import *
 class new_win():
     def __init__(self,long,tall):
         self.w = []
@@ -8,19 +7,54 @@ class new_win():
                 self.w[i].append(0)
         self.thing = []
         self.new = 1
-    def add(self,name,thing,x,y):
+        self.touch = []
+        self.touch_thing = []
+        self.name = {}
+        self.touch_name = []
+        self.big = []
+        
+    def add(self,thing,x,y):
+        self.thing.append(thing())
         s[x][y] = self.new
+        self.name[self.new] = thing().name 
         if thing.type == 's':
             for i in range(thing.x,0 - thing.x):
                 for t in range(thing.y,0 - thing.y):
-                    self.w[y - i][x - t] = self.new
+                    if self.w[y-i][x -t] == 0:
+                        self.w[y - i][x - t] = self.new
+                    else:
+                        self.touch.append([x-i,y-t])
+                        self.touch_name.append([thing.name(),self.name[s[x-i,y-t]]])
+        self.big.append(x*y)
+    def if_touch(self,x,y):
+        n = [x,y]
+        for i in self.touch:
+            if i[0] == n[0]:
+                if i[1] == n[1]:
+                    return true
+    def when_touch(self,onename,twoname):
         self.thing[name] = self.new
         self.new += 1
+        while True:
+            for u in self.touch_name:
+                if (u[0] == onename):
+                   if u[1] == twoname:
+                       break 
+                if (u[1] == onename):
+                    if u[0] == twoname:
+                        break
     def __str__(self):
         return self.w
+    def show(self):
+        pass
 class new_thing():
-    def s(self,x,y):
+    def __init__(self,x,y,name):
         self.type = 's'
         self.x = x 
         self.y = y
-        return self
+        self.name = name
+    def __str__(self):
+        return self.name
+if __name__ == '__main__':
+    win = new_win(100,100)
+    
